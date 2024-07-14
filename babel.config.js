2da4dda2fd09d21b4f0bff72dadde4f7649b9c5e
hassancodess <dev.hassancodess@gmail.com>
2024-07-14 18:07:21 +0500

    chore(config): minor changes to tamagui plugin config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
-
-  plugins.push([
-    '@tamagui/babel-plugin',
-    {
-      components: ['tamagui'],
-      config: './tamagui.config.ts',
-    },
-  ]);
-
   return {
     presets: ['babel-preset-expo'],
 
-    plugins,
+    plugins:[
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+    ],
   };
 };
