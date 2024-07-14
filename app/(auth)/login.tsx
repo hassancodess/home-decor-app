f19757622481b77ed08f8f7bc86c2a5b24f816c9
hassancodess <dev.hassancodess@gmail.com>
2024-07-14 18:18:16 +0500

    feat(auth): add auth screen

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const LoginPage = () => {
+  return (
+    <SafeAreaView>
+      <Text>LoginPage</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LoginPage;
+
+const styles = StyleSheet.create({});
