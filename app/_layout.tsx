d1066643fc9000383fac4c51491b6f263ca4e4e3
hassancodess <dev.hassancodes@gmail.com>
2024-07-17 17:22:26 +0500

    feat: remove hide splash screen hide on fonts load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
 import { TamaguiProvider } from 'tamagui';
 import {
   useFonts,
@@ -12,10 +11,12 @@ import {
 import tamaguiConfig from '~/tamagui.config';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useColorScheme } from 'react-native';
+import { AppProvider } from '~/context/AppContext';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
   const colorScheme = useColorScheme();
-
   const [loaded] = useFonts({
     Poppins: Poppins_400Regular,
     PoppinsLight: Poppins_300Light,
@@ -24,21 +25,17 @@ export default function Layout() {
     PoppinsBold: Poppins_700Bold,
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
   if (!loaded) return null;
 
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack screenOptions={{ headerShown: false, animation: 'fade' }}>
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="onboarding" />
-        </Stack>
+        <AppProvider>
+          <Stack screenOptions={{ headerShown: false, animation: 'fade' }}>
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="onboarding" />
+          </Stack>
+        </AppProvider>
       </ThemeProvider>
     </TamaguiProvider>
   );
