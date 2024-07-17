996cdfce1a74655b6b0f2c92a01b1b0bf3335e1e
hassancodess <dev.hassancodes@gmail.com>
2024-07-17 17:23:02 +0500

    feat: add hide splashscreen on isFirstTime check

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,14 @@
-import { Redirect } from 'expo-router';
+import { Redirect, SplashScreen } from 'expo-router';
+import Text from '~/components/shared/Text';
+import { useAppContext } from '~/context/AppContext';
 
 const Index = () => {
-  return <Redirect href="/onboarding" />;
+  const { isFirstTime } = useAppContext();
+  console.log('🚀 ~ Index ~ isFirstTime:', isFirstTime);
+  // <Text heading=''>asdasd</Text>
+  if (isFirstTime != null) {
+    SplashScreen.hideAsync();
+    return <Redirect href={isFirstTime ? '/onboarding' : '/login'} />;
+  }
 };
 export default Index;
