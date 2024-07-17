6b0e7db043021003198fb0ad57ad6149d4ecdef7
hassancodess <dev.hassancodes@gmail.com>
2024-07-17 17:33:17 +0500

    chore: cleanup

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
 import { Redirect, SplashScreen } from 'expo-router';
-import Text from '~/components/shared/Text';
 import { useAppContext } from '~/context/AppContext';
 
 const Index = () => {
   const { isFirstTime } = useAppContext();
   console.log('🚀 ~ Index ~ isFirstTime:', isFirstTime);
-  // <Text heading=''>asdasd</Text>
   if (isFirstTime != null) {
     SplashScreen.hideAsync();
     return <Redirect href={isFirstTime ? '/onboarding' : '/login'} />;
