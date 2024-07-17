5c807762bf3628d20e66632a06b4593f1fdef54f
hassancodess <dev.hassancodes@gmail.com>
2024-07-17 17:24:49 +0500

    feat: add _layout for auth screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from 'expo-router';
+import { getTokens, useTheme } from 'tamagui';
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const theme = useTheme();
+  const tokens = getTokens();
+  return (
+    <Stack
+      screenOptions={{
+        animation: 'simple_push',
+        headerTitleStyle: {
+          color: theme.primary.val,
+          fontSize: tokens.size.$1.val,
+        },
+        headerStyle: {
+          backgroundColor: 'transparent',
+        },
+      }}>
+      <Stack.Screen name="login" options={{ title: 'Login' }} />
+      <Stack.Screen name="register" options={{ title: 'Create Account' }} />
+    </Stack>
+  );
 }
