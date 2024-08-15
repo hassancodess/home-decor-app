be34b560d7fc2237074879b90e54dcc766cd48f7
hassancodess <dev.hassancodes@gmail.com>
2024-08-16 00:39:37 +0500

    chore: add profile type

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,13 @@ export type TOnboardingItem = {
   image: string;
   description: string;
 };
+
+export type TProfile = {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  dateOfBirth: Date;
+  gender: 'male' | 'female';
+  avatarUrl: string;
+};
